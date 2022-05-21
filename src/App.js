f8535651928814509d99861354bda116be67d661
Derek Baker <derek.baker89@gmail.com>
2022-05-21 02:23:55 +0100

    Reformatted with prettier

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import Header from './components/Header'
 
 
 function App() {
-    return(
+    return (
         <>
-            <Header />
+            <Header/>
             <div className={'container'}>
                 <h1>My App</h1>
             </div>
