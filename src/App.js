d3b5795daea38ff455284ad3686d1b426ed1d089
Derek Baker <derek.baker89@gmail.com>
2022-06-10 02:23:24 +0100

    Average stats component for feedback displayed on top of all feedback

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from './components/Header'
 import {useState} from "react";
 import FeedbackData from "./data/feedbackData";
 import FeedbackList from "./components/FeedbackList";
+import FeedbackStats from "./components/FeedbackStats";
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
         <>
             <Header/>
             <div className={'container'}>
+                <FeedbackStats feedback={feedback}/>
                 <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
             </div>
         </>
