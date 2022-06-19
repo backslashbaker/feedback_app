acbf2fd66b4a46ada783a13b4bd9635579f92bb8
Derek <derek.baker89@gmail.com>
2022-06-19 22:50:51 +0100

    add feedback to global state

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from 'uuid'
 import Header from './components/Header'
 import {useState} from "react";
 import FeedbackData from "./data/feedbackData";
@@ -9,6 +10,11 @@ import FeedbackForm from "./components/FeedbackForm";
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData)
 
+    const addFeedback = (newFeedback) => {
+        newFeedback.id = uuidv4()
+        setFeedback([newFeedback, ...feedback])
+    }
+
     const deleteFeedback = (id) => {
         if (window.confirm('Are you sure you want to delete?')) {
             setFeedback(feedback.filter((item) => item.id !== id))
@@ -19,7 +25,7 @@ function App() {
         <>
             <Header/>
             <div className={'container'}>
-                <FeedbackForm/>
+                <FeedbackForm handleAdd={addFeedback}/>
                 <FeedbackStats feedback={feedback}/>
                 <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
             </div>
